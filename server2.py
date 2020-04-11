222db897573f9f8e0645727cf49a63298769b45e
Jimmy Zhao <zhao.jimmy@hotmail.com>
2020-04-11 13:07:10 -0400

    add socket codes

--- server2.py
+++ server2.py
@@ -1,4 +1,8 @@
 import socket
+import time
+import pickle
+
+HEADERSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
@@ -8,5 +12,8 @@ while True:
     # now our endpoint knows about the OTHER endpoint.
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established.")
-    clientsocket.send(bytes("Hey there!!!", "utf-8"))
-    clientsocket.close()
+
+    d = {1: "Hey", 2: "There"}
+    msg = pickle.dump(d)
+    msg = bytes(f"{len(msg):<{HEADERSIZE}}", "utf-8") + msg
+    clientsocket.send(msg)
--- server3.py
+++ server3.py
@@ -1,4 +1,9 @@
 import socket
+import time
+import pickle
+
+
+HEADERSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
@@ -8,5 +13,9 @@ while True:
     # now our endpoint knows about the OTHER endpoint.
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established.")
-    clientsocket.send(bytes("Hey there!!!", "utf-8"))
-    clientsocket.close()
+
+    d = {1: "hi", 2: "there"}
+    msg = pickle.dumps(d)
+    msg = bytes(f"{len(msg):<{HEADERSIZE}}", "utf-8") + msg
+    print(msg)
+    clientsocket.send(msg)
